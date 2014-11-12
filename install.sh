ce675259df5a099f464e4af1773160b856873946
rgrannell1 <r.grannell2@gmail.com>
2014-11-12 21:15:19 +0000

    Updated installer

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+here=$(pwd)
 
-
+cd ~
 sudo npm-install -g travis-ci
+
+rm -rf cistatus
+git clone https://github.com/rgrannell1/ci-status.git
+cd cistatus
+
+echo alias swatted='node "~/cistatus/lib/docopt-cistatus.js"' >> ~/.bashrc && . ~/.bashrc
+
+cd $here
